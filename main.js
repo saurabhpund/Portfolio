4a7359ef3ffacccdca6170a4f64b96df00d763d4
Saurabh Pund <80202824+saurabhpund@users.noreply.github.com>
2021-07-13 23:43:07 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,16 @@
-$(document).ready(function(){
+let menu = document.querySelector("#menu");
 
+menu.addEventListener("click", ()=>{
+    if(menu.className== "fa fa-close"){
+        document.getElementById("header").style.left = "-120%";
+        menu.className = " fa fa-bars";
+    }
+    else{
+        
+        document.querySelector("#header").style.left = "0%"
+        menu.className = "fa fa-close";
 
-    $('#menu').click(function(){
-        $(this).toggleClass('fa fa-times');
-        $('header').toggleClass('toggle');
-    })
-})
+    }
+    
+});
+
