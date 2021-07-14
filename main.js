a0d41955ed6b0683dbedacc151c46dd0abe2b710
Saurabh Pund <80202824+saurabhpund@users.noreply.github.com>
2021-07-14 09:35:55 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,16 +1,21 @@
 let menu = document.querySelector("#menu");
+let header = document.querySelector("#header");
 
-menu.addEventListener("click", ()=>{
-    if(menu.className== "fa fa-close"){
-        document.getElementById("header").style.left = "-120%";
-        menu.className = " fa fa-bars";
+if (screen.width <= 991) {
+  console.log("Active");
+  menu.style.display = "block";
+  menu.addEventListener("click", () => {
+    if (menu.className == "fa fa-bars") {
+      header.style.left= "0%";
+      menu.className = "fa fa-close";
+    } else {
+      header.style.left = "-120%";
+      menu.className = "fa fa-bars";
     }
-    else{
-        
-        document.querySelector("#header").style.left = "0%"
-        menu.className = "fa fa-close";
+  });
+} else {
+  menu.style.display = "none";
+  header.style.left = "0%";
+}
 
-    }
-    
-});
 
